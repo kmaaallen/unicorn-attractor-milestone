e1fa6938012f28332342fd4472628d39a18b9bbe
Kirsten <kmaaallen@gmail.com>
2019-12-28 12:05:32 +0000

    fixed typo

--- accounts/url_reset.py
+++ accounts/url_reset.py
@@ -6,6 +6,6 @@ from django.contrib.auth.views import password_reset, password_reset_complete, p
 urlpatterns = [
     url(r"^$", password_reset, {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
     url(r"^done/$", password_reset_done, name="password_reset_done"),
-    url(r"^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$", password_reset_confirm, {'post_reset_confirm': reverse_lazy('password_reset_complete')}, name = "password_reset_confirm"),
+    url(r"^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$", password_reset_confirm, {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name = "password_reset_confirm"),
     url(r"^complete/$", password_reset_complete, name="password_reset_complete"),
 ]
