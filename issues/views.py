08fd2a52f57607e1aee4a9cebba44a296625cc5f
Kirsten <kmaaallen@gmail.com>
2020-01-26 21:40:56 +0000

    fixing upvote url in full issue page, some styling

--- issues/views.py
+++ issues/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Issue, Vote, Comment
 from .forms import AddIssueForm, AddCommentForm
 from django.contrib.auth.decorators import login_required
