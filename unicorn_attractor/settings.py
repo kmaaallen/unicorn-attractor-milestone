ffc088db1df3f7d43ef8bc366a10d8a961a92203
Kirsten <kmaaallen@gmail.com>
2020-04-19 12:44:30 +0000

    Debug turned off

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_LOCATION = 'static'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
